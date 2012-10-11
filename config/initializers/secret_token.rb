51555a353b1cd34c3f1499f77695bdbb19d29ecf
Chris Clayton <chris.clayton@vocalink.com>
2012-10-11 12:19:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnixWeb::Application.config.secret_token = '73dd71703bc2fc001621c0026ef3e4d2ff97f4c194482a592e3cb419d56675346f7310e9765598162c84ccbaad63b9b0f130098ac7386145a2fd74b86e1ebf3e'
